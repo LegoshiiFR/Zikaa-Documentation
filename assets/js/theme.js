9e1cb95f9cdebaac767615d50e7b36e0b5112a50
Legoshii <youtune.contact.rageman@gmail.com>
2022-10-16 20:15:28 +0200

    Update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,21 +1,11 @@
-/*
-================================================================
-* Template:  	 iDocs - One Page Documentation HTML Template
-* Written by: 	 Harnish Design - (http://www.harnishdesign.net)
-* Description:   Main Custom Script File
-================================================================
-*/
-
-
 (function ($) {
 	"use strict";
 
 // Preloader
-$(window).on('load', function () {
-	$('.lds-ellipsis').fadeOut(); // will first fade out the loading animation
-	$('.preloader').delay(333).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(333);
-});
+var preloader = document.getElementById('loader');
+      function preLoaderHandler(){
+          preloader.style.display = 'none';
+      }
 
 /*-------------------------------
     Primary Menu
